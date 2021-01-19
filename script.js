6461af742b690d532d90eebdd3200b6301cc5997
AnshumanPradhan009 <70283200+AnshumanPradhan009@users.noreply.github.com>
2021-01-19 19:08:05 +0530

    Mastered

---
+++ script.js
@@ -0,0 +1,17 @@
+function clock() {
+    var hours = document.getElementById('hour');
+    var minutes = document.getElementById('minute');
+    var seconds = document.getElementById('second');
+
+    var h = new Date().getHours();
+    var m = new Date().getMinutes();
+    var s = new Date().getSeconds();
+
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+
+    setInterval(clock, 1000);
+};
+
+clock();
